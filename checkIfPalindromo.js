10ef330fc51707998d52c4b13db4882f072cb359
Martim Tiburcio <157258683+tiburcioMartim@users.noreply.github.com>
2024-03-05 06:58:16 -0300

    Update checkIfPalindromo.js

--- checkIfPalindromo.js
+++ checkIfPalindromo.js
@@ -1,3 +1,16 @@
 function isPalindromo(frase) {
-    
-}
+    const fraseFormatada = frase.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+    for (let i = 0; i < fraseFormatada.lenght; i++){
+        if (fraseFormatada[i] !== fraseFormatada[fraseFormatada.lenght -1 -i]) {
+            return false;
+        };
+    };
+    return true;
+};
+
+const frase1 = 'Arara';  //true
+const frase2 = 'Coelho'; //false
+
+console.log(isPalindromo(frase1));
+console.log(isPalindromo(frase2));
