99bf4046d1c86aaead06c00cb181e027390f930a
tiburcioMartim <tiburciomartim@gmail.com>
2024-03-08 17:03:49 -0300

    refresh

--- lib.js
+++ lib.js
@@ -12,16 +12,48 @@ function isPrime(n) {
     return n;
 };
 
+//print all primes
+function counterIsPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (isPrime(i) !== false) {
+            console.log(i);
+        };
+    };
+    return '';
+};
+
+//Checks if a number is a twin prime and print in the screen the numbers prime
+function isPrimeTwin(n) {
+    let p1 = 0;
+    let p2 = 0;
+
+    for (let i = 3; i < n; i++) {
+        if (isPrime(i) !== false) {
+            p1 = i - p2;
+            p2 = i;
+            if (p1 === 2) {
+                console.log(`Primos gêmeos: ${i - 2} e ${p2}`);
+            };
+        };
+    };
+    return '';
+};
+
 //Check if a string is palindromo
 function isPalindromo(s) {
     const stringFomated = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
 
-    for (let i = 0; i < stringFomated.lenght / 2; i++){
-        if (stringFomated[i] !== stringFomated[stringFomated.lenght -1 -i]) {
+    for (let i = 0; i < stringFomated.lenght / 2; i++) {
+        if (stringFomated[i] !== stringFomated[stringFomated.lenght - 1 - i]) {
             return false;
         };
     };
     return true;
 };
 
-module.exports = { isPrime, isPalindromo };
+
+module.exports = {
+    isPrime,
+    isPrimeTwin,
+    isPalindromo
+};
--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-//  { isPrimeTwin }   Checks if a number is a twin prime
-// identificar o primo gemeo 2 caracters imediatamente ao lado. Exe.: 11 e 13
 const { isPrime } = require("./lib")
 
-function isPrimeTwin(n) {
-    let p1 = 0;
-    let p2 = 0;
 
-    for (let i = 3; i < n; i++) {
-        if (isPrime(i) !== false) {
-            p1 = i - p2;
-            p2 = i;
-            if (p1 === 2) {
-                console.log(`Primos gêmeos: ${i - 2} e ${p2}`);
-            };
-        };
-    };
-};
 
 console.log(isPrimeTwin(50))
